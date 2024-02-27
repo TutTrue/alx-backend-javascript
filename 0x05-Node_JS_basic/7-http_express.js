b13f42904b276814b353dddfe9892bfb6b725fbf
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-27 23:03:23 +0200

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -6,10 +6,13 @@ const app = express();
 app.get('/', (req, res) => res.send('Hello Holberton School!'));
 app.get('/students', (req, res) => {
   const path = process.argv[2];
+  let studentsData = '';
+  studentsData += 'This is the list of our students\n';
   countStudents(path)
     .then((data) => {
+      studentsData += data;
       res.setHeader('Content-Type', 'text/plain');
-      res.end(data);
+      res.end(studentsData);
     })
     .catch(() => {
       res.setHeader('Content-Type', 'text/plain');
