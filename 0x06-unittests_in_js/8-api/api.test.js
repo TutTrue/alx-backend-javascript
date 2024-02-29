3d6007ec0de8c047bc55239c0c85ed34e7e237b9
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-03-01 01:21:29 +0200

    Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,15 +1,13 @@
-const mocha = require('mocha');
+const expect = require('chai').expect;
 const request = require('request');
 
 describe('GET /', () => {
   it('should return a string', () => {
     request.get('http://localhost:7865', (err, res, body) => {
       if (err) throw err;
-      if (res.statusCode === 200) {
-        if (body === 'Welcome to the payment system') {
-          return true;
-        }
-      }
+      expect(body).to.be.equal('Welcome to the payment system');
+      expect(res.statusCode).to.be.equal(200);
+      expect(typeof body).to.be.equal('string');
     });
   });
 });
