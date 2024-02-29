adbd82b7080be18dd39dbca8548c0841ecf3a2b9
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-29 21:25:13 +0200

    use chai  assertion lib

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,24 @@
+const calculateNumber = require('./2-calcul_chai.js');
+const chai = require('chai');
+const excpect = chai.expect;
+
+describe('Calcul', () => {
+  it('should return 4', () => {
+    excpect(calculateNumber('SUM', 1, 3)).to.equal(4);
+  });
+
+  it('should return 5', () => {
+    excpect(calculateNumber('SUM', 1.5, 3)).to.equal(5);
+  });
+
+  it('should return -4', () => {
+    excpect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+  });
+
+  it('should return 0.2', () => {
+    excpect(calculateNumber('DIVIDE', 1, 5)).to.equal(0.2);
+  });
+  it('should return error', () => {
+    excpect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+  });
+});
